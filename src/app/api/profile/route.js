ac784f7676645e42b100bb56cbade5caccf6e338
Alireza <alirezakhaksar.freelancer@gmail.com>
2024-02-02 00:35:11 +0330

    buy-residential

--- src/app/api/profile/route.js
+++ src/app/api/profile/route.js
@@ -4,6 +4,23 @@ import { Types } from "mongoose";
 import connectDB from "@/utils/connectDB";
 import Profile from "@/models/Profile";
 
+export async function GET() {
+  try {
+    await connectDB();
+
+    const profiles = await Profile.find().select("-userId");
+
+    return Response.json(
+      {
+        data: profiles,
+      },
+      { status: 200 }
+    );
+  } catch (err) {
+    console.log(err);
+    return Response.json({ error: "مشکلی در سرور رخ داده است" }, { status: 500 });
+  }
+}
 export async function POST(req) {
   try {
     await connectDB();
@@ -85,7 +102,8 @@ export async function PATCH(req) {
 
     const profile = await Profile.findOne({ _id });
 
-    if(!user._id.equals(profile.userId)) Response.json({ error: "دسترسی شما به این اگهی مسدود شده است !" }, { status: 403 });
+    if (!user._id.equals(profile.userId))
+      Response.json({ error: "دسترسی شما به این اگهی مسدود شده است !" }, { status: 403 });
 
     profile.title = title;
     profile.description = description;
@@ -99,7 +117,6 @@ export async function PATCH(req) {
     profile.category = category;
     profile.save();
     return Response.json({ message: "آگهی با موفقیت ویرایش شد" }, { status: 200 });
-
   } catch (err) {
     console.log(err);
     return Response.json({ error: "مشکلی در سرور رخ داده است !", status: 500 });
---
+++ src/components/templates/BuyResidentialPage.js
@@ -0,0 +1,24 @@
+import Sidebar from "@/modules/Sidebar";
+import Card from "@/modules/Card";
+import styles from "@/templates/BuyResidentialsPage.module.css";
+
+function BuyResidentialsPage({ data }) {
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.sidebar}>
+        <Sidebar />
+      </div>
+      <div className={styles.main}>
+        {data.length ? null : (
+          <p className={styles.text}>هیچ آگهی ثبت نشده است</p>
+        )}
+        {data.map((profile) => (
+          <Card key={profile._id} data={profile} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default BuyResidentialsPage;
