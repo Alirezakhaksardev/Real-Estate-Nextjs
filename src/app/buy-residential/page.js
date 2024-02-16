3ec94a1166fd8bcaf58a5f58cb1c73c1dc7d85dc
Alireza <alirezakhaksar.freelancer@gmail.com>
2024-02-16 10:02:03 +0330

    Update buy-res page

--- src/app/buy-residential/page.js
+++ src/app/buy-residential/page.js
@@ -1,14 +1,19 @@
+import Profile from "@/models/Profile";
 import BuyResidentialsPage from "@/templates/BuyResidentialPage";
+import connectDB from "@/utils/connectDB";
 import React from "react";
 
 async function page({ searchParams }) {
-  const res = await fetch("http://localhost:3000/api/profile", {
-    cache: "no-store",
-  });
-  const data = await res.json();
-  if (data.error) return <h3>مشکلی پیش آمده است</h3>;
+  // const res = await fetch("http://localhost:3000/api/profile", {
+  //   cache: "no-store",
+  // });
+  // const data = await res.json();
+  await connectDB();
+  const profiles = await Profile.find({ published: true });
 
-  let finalData = data.data;
+  if(!profiles) return <h3>مشکلی پیش آمده است</h3>;
+
+  let finalData = profiles;
   if (searchParams.category) {
     finalData = finalData.filter((i) => i.category === searchParams.category);
   }
